a40452d3e83f697370ea3a289c5241b9c0620312
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-11-08 20:43:08 +0100

    crud etudiant complet

--- src/sipacademy_project/app/Main.java
+++ src/sipacademy_project/app/Main.java
@@ -18,19 +18,28 @@ public class Main {
 		// System.out.println("Connexion réussi : " +con);
 
 		// Lecture des paramètres à partir du clavier
-		/*
-		Scanner sc = new Scanner(System.in); // System.in : lecture du clavier
 		
-		System.out.println("Donner un nom");
-		String nom = sc.nextLine();
+		//Scanner sc = new Scanner(System.in); // System.in : lecture du clavier
 		
-		System.out.println("Donner la classe");
-		String classe = sc.nextLine();
-		*/
+		//System.out.println("Donner un nom");
+		//String name = sc.nextLine();
+		
+		//System.out.println("Donner la classe");
+		//String classe = sc.nextLine();
+		
 		Statement stmt = con.createStatement();
 		//int result = stmt.executeUpdate("insert into etudiant(nom,classe) values('Mahfoudh', '3 ieme')");
 		//int result = stmt.executeUpdate("insert into etudiant(nom,classe) values('"+nom+"', '"+classe+"')");
 		//System.out.println(result);
+		
+		
+		// STEP 2 : Exemple de Update
+		
+		//stmt.executeUpdate("update etudiant set nom='"+name+"' where id='1'");
+		
+		// STEP 5 : Delete
+		
+		stmt.executeUpdate("delete from etudiant where id='2'");
 		
 		// STEP 3: Création objet ResultSET(table en mémoire) dont le rôle est de stocker le résultat d'une requete select
 		ResultSet rs = stmt.executeQuery("select id, nom, classe from etudiant");
